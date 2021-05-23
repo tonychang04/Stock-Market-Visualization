8ebe2db04567cfe22d3531f49998172efeeff617
tony430 <tonychang430@hotmail.com>
2021-05-23 20:50:53 +0800

    Graphed line graph of a single company

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import numpy as np
 
 
 if __name__ == '__main__':
+    companies = []
     aapl = data.DataReader("AAPL",
                            start='2020-1-1',
                            end='2020-12-31',
                            data_source='yahoo')['Adj Close']
 
+    print(aapl.index)
 
-    print(aapl.to_frame().head())
+    plt.plot(aapl.index, aapl.values)
+    plt.show(block=True)
+    plt.interactive(True)
